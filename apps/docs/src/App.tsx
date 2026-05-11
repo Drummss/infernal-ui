d83397caa1cf6f00606ecb537079b41984b45403
Drummss <1509172+Drummss@users.noreply.github.com>
2026-05-11 01:59:01 +0100

    docs: title and favicon

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,4 +1,5 @@
 import { InfernalContext } from '@infernal-ui/solid';
+import { Meta, MetaProvider, Title } from '@solidjs/meta';
 import { Navigate, Route, Router } from '@solidjs/router';
 import { type Component, lazy } from 'solid-js';
 import { DocsLayout, defaultDocsHref, docsRoutes, HomeLayout } from './layouts';
@@ -8,26 +9,31 @@ const DocsPage = lazy(() => import('./pages/docs-page'));
 
 const App: Component = () => {
   return (
-    <InfernalContext defaultTheme="dark">
-      <Router>
-        <Route path="/" component={HomeLayout}>
-          <Route path="/" component={HomePage} />
-        </Route>
-        <Route path="/docs" component={DocsLayout}>
-          <Route
-            path="/"
-            component={() => <Navigate href={defaultDocsHref} />}
-          />
-          {docsRoutes.map(({ path, page }) => (
-            <Route path={path} component={() => <DocsPage page={page} />} />
-          ))}
-          <Route
-            path="*missing"
-            component={() => <Navigate href={defaultDocsHref} />}
-          />
-        </Route>
-      </Router>
-    </InfernalContext>
+    <MetaProvider>
+      <Title>Infernal UI</Title>
+      <Meta name="description" content="Infernal UI Documentation" />
+
+      <InfernalContext defaultTheme="dark">
+        <Router>
+          <Route path="/" component={HomeLayout}>
+            <Route path="/" component={HomePage} />
+          </Route>
+          <Route path="/docs" component={DocsLayout}>
+            <Route
+              path="/"
+              component={() => <Navigate href={defaultDocsHref} />}
+            />
+            {docsRoutes.map(({ path, page }) => (
+              <Route path={path} component={() => <DocsPage page={page} />} />
+            ))}
+            <Route
+              path="*missing"
+              component={() => <Navigate href={defaultDocsHref} />}
+            />
+          </Route>
+        </Router>
+      </InfernalContext>
+    </MetaProvider>
   );
 };
 
